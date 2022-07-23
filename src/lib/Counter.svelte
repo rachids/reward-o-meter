062d457b7962cee21e008cdb35d975e261bab29f
rachids <livecoding@rachids.ca>
2022-07-22 21:25:01 -0400

    add twurple + animation

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -2,6 +2,22 @@
   let count = 0
   const increment = () => {
     count += 1
+
+    let body = {
+      envPrivate: import.meta.env.TEST,
+      envPublic: import.meta.env.VITE_TEST,
+      test: 'coucou'
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
   }
 </script>
 
---
+++ src/lib/Thermometer.svelte
@@ -0,0 +1,117 @@
+<script>
+    import { fade, fly } from 'svelte/transition';
+
+    /*export let client;
+
+    let test = '';
+
+    if (!client.isConnected()) {
+        throw 'Client is not connected to Twitch';
+    }
+
+    const increment = (message) => {
+        test = `${message.userDisplayName} a chopé ${message.rewardTitle}`;
+    }
+
+    const pubSubClient = client.listen();
+
+    pubSubClient.onRedemption(client.userId, (message) => {
+        increment(message);
+    });*/
+    let score = 0;
+    let dropIsVisible = false;
+
+    const increment = () => {
+        dropIsVisible = true;
+        score += 10;
+    }
+
+    const resetDrop = () => {
+        const divGlass = document.getElementsByClassName("glass")[0];
+        const divLiquid = document.getElementsByClassName("liquid")[0];
+        const root = document.querySelector(":root");
+        let amountInPercentage = 10;
+        dropIsVisible = false;
+
+        // on ajoute dans le thermomètre
+        const calcPourcent = (amountInPercentage * divGlass.clientHeight) / 100;
+        const totalToAdd = divLiquid.clientHeight + calcPourcent;
+        root.style.setProperty("--liquid-height", totalToAdd + "px");
+        score = (totalToAdd / divGlass.clientHeight) * 100;
+    };
+</script>
+
+<div class="glass">
+    <div class="liquid">
+        {#if dropIsVisible}
+            <div id="drop"
+                 in:fly={{y: -300, duration: 4000}}
+                 on:introend={resetDrop}
+                 out:fade
+            >🩸</div>
+        {/if}
+    </div>
+    <div class="label">
+        OBJECTIF:
+        <span id="score">{score}</span>
+        %
+    </div>
+</div>
+
+<button on:click={increment} disabled={dropIsVisible}>Click</button>
+
+<style>
+    :root {
+        --glass-width: 40px;
+        --font-size-drop: 20px;
+        --glass-height: 300px;
+        --liquid-height: 30px;
+        --addanimation: 2px;
+    }
+
+    body {
+        background-color: transparent;
+    }
+
+    .glass {
+        width: var(--glass-width);
+        height: var(--glass-height);
+        border: 3px solid gray;
+        border-radius: 0 0 25px 25px;
+        border-top: 0;
+        display: flex;
+        align-items: flex-end;
+        position: relative;
+        margin: auto;
+    }
+
+    .liquid {
+        width: 90%;
+        height: var(--liquid-height);
+        margin-left: 5%;
+        margin-bottom: 5%;
+        background-color: red;
+        overflow: hidden;
+        border-radius: 0 0 25px 25px;
+        transition: height .5s linear;
+    }
+
+    .label {
+        width: 300px;
+        transform: rotate(-90deg);
+        position: absolute;
+        left: -78px;
+        right: 0;
+        bottom: 164px;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+
+    /* Animation goute  */
+    #drop {
+        font-size: var(--font-size-drop);
+        position: absolute;
+        z-index: 2;
+        top: 250px;
+        left: 10px;
+    }
+</style>
